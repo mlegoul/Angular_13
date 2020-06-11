66d62103da8df3e065705f944798b48e92403fff
mehdi <legoullonmehdi@gmail.com>
2020-06-11 22:03:01 +0200

    improve ux

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,6 +2,9 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home.component';
 import {HomeRoutingModule} from './home-routing.module';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MessageModule} from '../../components/message/message.module';
 
 
 @NgModule({
@@ -9,6 +12,9 @@ import {HomeRoutingModule} from './home-routing.module';
   imports: [
     CommonModule,
     HomeRoutingModule,
+    MatCardModule,
+    MatGridListModule,
+    MessageModule,
   ]
 })
 export class HomeModule {
